7fcdb029598716c5e89b6afc07f76e37fb0dad8a
John E <linux@8192.net>
2011-07-27 04:54:51 -0700

    Correct switch id.

--- zpdprojector.pl
+++ zpdprojector.pl
@@ -7,5 +7,5 @@ use zpdapp;
 
 my $zpd = zpdapp->new();
 
-my $r = $zpd->send_recv( "switch projector id 1 value 5" );
+my $r = $zpd->send_recv( "switch projector id 2 value 5" );
 exit $r->{power}->{switch_data};
